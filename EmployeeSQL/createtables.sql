5623e5071bbd7980817e2d7c4ece7dbca8f9ca37
krfennel <70558076+krfennel@users.noreply.github.com>
2020-11-15 19:37:38 -0500

    Final

--- EmployeeSQL/createtables.sql
+++ EmployeeSQL/createtables.sql
@@ -1,13 +1,16 @@
+-- 1. Titles Table - 1st
 create table titles (
     title_id        varchar(6)     primary key,
     title           varchar(50)    not null
 );
 
+-- 2. Departments Table - 2nd
 create table departments (
     dept_no        varchar(6)     primary key,
     dept_name      varchar(50)    not null
 );
 
+-- 3. Employees Table - 3rd
 create table employees (
     emp_no          int            unique not null,
     emp_title_id    varchar(6)     not null,
@@ -20,11 +23,13 @@ create table employees (
 	foreign key (emp_title_id) references titles(title_id)
 );
 
+-- 4. Salaries Table - 4th
 create table salaries (
     emp_no      int     		primary key,
     salary      decimal(10,0)   not null
 );
 
+-- 5. Department Employees table - 5th
 create table department_employee (
     emp_no       int     		not null,
     dept_no      varchar(6)    	not null,
@@ -33,6 +38,7 @@ create table department_employee (
 	foreign key (emp_no) references employees(emp_no)
 );
 
+-- 6. Department Managers Tables - 6th
 create table department_manager (
     dept_no     varchar(6)     	not null,
     emp_no		int				not null,
